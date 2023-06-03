45be889f4c88e26594bc50cb740c534fc250a0b6
issiakhem ramy <134652499+issiakhemramy@users.noreply.github.com>
2023-06-03 20:42:39 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>choose your guitar</title>
+    <link rel="stylesheet" href="index.css">
+    <script src="script.js"></script>
+
+</head>
+<body>
+
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">musicsr</h2>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    
+                    <li><a href="espanol.html">spanish</a></li>
+                    <li><a href="jazz.html">jazz</a></li>
+                    <li><a href="rock.html">rock</a></li>
+                    <li><a href="classic.html">classical</a></li>
+                </ul>
+            </div>
+
+            <div class="search">
+                <input class="srch" type="search" name="" placeholder="Type To text">
+                <a href="#"> <button class="btn">Search</button></a>
+            </div>
+
+        </div> 
+        <div class="content">
+            <h1>What's the best<br><span>guitar</span> <br>for You ?</h1>
+            <p class="par">The guitar is a popular stringed instrument played by strumming or picking its strings with 
+                the finger<br> or a pick. Proper technique and regular practice are important to play it effectively. 
+                It can be enjoyed <br>  as a solo activity or in a band or ensemble.</p>
+        
+                <button class="cn"><a href="#">TRY IT !</a></button>
+
+                <div class="form">
+                    <h2>Login Here</h2>
+                    <input type="email" name="email" placeholder="Enter Email Here">
+                    <input type="password" name="" placeholder="Enter Password Here">
+                    <button class="btnn"><a href="#">Login</a></button>
+
+                    <p class="link">Don't have an account<br>
+                    <a href="#">Sign up </a> here</a></p>
+                    <p class="liw">Log in with</p>
+
+                    <div class="icons">
+                        <a href="#"><ion-icon name="logo-facebook"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-instagram"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-twitter"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-google"></ion-icon></a>
+                        <a href="#"><ion-icon name="logo-skype"></ion-icon></a>
+                    </div>
+
+                </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+</body>
+</html>
